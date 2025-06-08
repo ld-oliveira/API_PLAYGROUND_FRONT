3354b5a3ecd9d05aeb7188bb499308154b31f71a
ld-oliveira <ld-oliveira@uol.com>
2025-06-08 20:31:48 -0300

    organização do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/components/Header.scss';
+import Logo2 from '../assets/images/Logo (2).png';
 
 const Header = () => {
   return (
     <header className='header'>
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/API 1">API 1</Link></li>
-          <li><Link to="/API 2">API 2</Link></li>
-          <li><Link to="/API 3">API 3</Link></li>
-          <li><Link to="/API 4">API 4</Link></li>
-          <li><Link to="/API 5">API 5</Link></li>
-          <li><Link to="/contato">Sobre mim</Link></li>
-        </ul>
+          <div className="nav-buttons">
+          <Link to="/"><button>Home</button></Link>
+          <Link to="/API 1"><button>API 1</button></Link>
+          <Link to="/API 2"><button>API 2</button></Link>
+          <Link to="/API 3"><button>API 3</button></Link>
+          <Link to="/API 4"><button>API 4</button></Link>
+          <Link to="/API 5"><button>API 5</button></Link>
+          <Link to="/contato"><button>Sobre mim</button></Link>
+          </div>
+
+          <div classname="logo-area"> 
+          <img src={Logo2} alt="Logo" className="logo"/>
+        </div>
+        
       </nav>
     </header>
   );
