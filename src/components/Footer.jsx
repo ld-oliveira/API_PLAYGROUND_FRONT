c2668428327d176cb2af6e6fc8d0b378a69bfa99
ld-oliveira <ld-oliveira@uol.com>
2025-07-15 02:44:06 -0300

    adicionando mouse over em componentes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import '../styles/components/Footer.scss';
-
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-      <p>&copy; API Playground, by Léo oliveira</p>
+    <footer className="footer">
+      <p>© API Playground, by Léo Oliveira</p>
+      <div className="footer__icons">
+        <a href="https://github.com/ld-oliveira" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/leo-b-oliveira/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="ld-oliveira@uol.com.br">
+          <FaEnvelope />
+        </a>
+      </div>
     </footer>
   );
 };
