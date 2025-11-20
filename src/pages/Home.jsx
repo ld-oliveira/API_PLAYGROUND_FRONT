b995261a2e54775be01dc0c724467392f5bd7d77
ld-oliveira <ld-oliveira@uol.com>
2025-11-20 01:27:40 -0300

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,9 +8,9 @@ const Home = () => {
       <div className="descricao">
         <h1 className="titulo-home">Resumo do meu projeto</h1>
         <p className="descricao_do_projeto">
-          O propósito desse projeto é simples: <strong>aprender na prática.</strong> Estou trabalhando com React no front e Django no back, explorando algumas APIs completamente aleatórias apenas pra simular diferentes cenários com necessidades e metodos distintos. 
-          A ideia é treinar o front (sem focar muito no visual por enquanto), e no back-end, autenticação, deploy, versionamento e todo tipo de bugs que forem aparecendo no caminho. (não estão sendo poucos /cry )
-          Com o tempo vou melhorando e adicionando novas funcionalidades e deixando tudo mais redondinho. Aqui em cima tem links de algumas funcionalidades que estão funcionando (espero), 
+          O propósito desse projeto é simples: <strong>aprender na prática.</strong> Estou trabalhando com React no front e Django no back, explorando algumas APIs para simular diferentes cenários com necessidades e metodos distintos.
+          A ideia é treinar o front (sem focar muito no visual por enquanto e sim na estrutura, organização e legibilidade), e no back-end o foco atual é trabalhar autenticação, deploy, versionamento e todos os tipo de bugs e impecilios que forem aparecendo no caminho.
+          Com o tempo vou melhorando e adicionando novas funcionalidades e deixando tudo mais redondinho. Aqui em cima tem links de algumas funcionalidades que estou trabalhando,
           tudo com o objetivo de evoluir minhas habilidades e me adaptar a diferentes contextos.
           Se tiver ideias, sugestões, achar algum bug ou só quer jogar conversa fora, nesse  <Link className='link-contato' to="/contato"> link aqui</Link> tem várias formas de falar comigo. Me chama lá! 😉
         </p>
